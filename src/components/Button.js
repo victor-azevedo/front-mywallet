fcc8cc5388de7549a0f5db08446dae8a2b91eaf4
Victor Azevedo <josevcazevedo@gmail.com>
2023-05-25 19:32:01 -0300

    refactor: add basic button component

---
+++ src/components/Button.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+
+import { accentColor, buttonTextColor } from "../constants/colors";
+
+export default function Button({ children, ...rest }) {
+  return <ButtonStyled {...rest}>{children}</ButtonStyled>;
+}
+
+const BUTTON_BG_COLOR = accentColor;
+const BUTTON_COLOR = buttonTextColor;
+
+const ButtonStyled = styled.button`
+  width: 100%;
+  background-color: ${BUTTON_BG_COLOR};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  margin: 16px 0;
+  padding: 16px 12px;
+  color: ${BUTTON_COLOR};
+  font-size: 20px;
+  font-weight: 700;
+  border-radius: 5px;
+  border-style: none;
+  box-shadow: rgba(0, 0, 0, 0.45) 0px 20px 15px -15px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  &:active {
+    box-shadow: rgba(0, 0, 0, 0.45) 0px 5px 15px -5px;
+  }
+  &:disabled {
+    opacity: 0.7;
+    box-shadow: rgba(0, 0, 0, 0.45) 0px 5px 15px -15px;
+  }
+`;
--- src/constants/colors.js
+++ src/constants/colors.js
@@ -3,8 +3,11 @@ export const accentColor = "#A328D6";
 
 export const textColor = "#000000";
 export const lightTextColor = "#C6C6C6";
+
 export const inputTextColor = "#000000";
 export const labelTextColor = "#FFF";
+
+export const buttonTextColor = "#FFF";
 
 export const incomingColor = "#03AC00";
 export const outgoingColor = "#C70000";
